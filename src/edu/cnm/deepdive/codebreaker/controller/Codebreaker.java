935dd920f5a8ff3a6e6bd70830f1868d7118f070
isaac <shifdub@gmail.com>
2020-10-09 09:37:31 -0600

    Started Javadoc comments

--- src/edu/cnm/deepdive/codebreaker/controller/Codebreaker.java
+++ src/edu/cnm/deepdive/codebreaker/controller/Codebreaker.java
@@ -2,42 +2,64 @@ package edu.cnm.deepdive.codebreaker.controller;
 
 import edu.cnm.deepdive.codebreaker.model.Code.Guess;
 import edu.cnm.deepdive.codebreaker.model.Game;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.security.SecureRandom;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
 
+/**
+ * Implements a simple console-mode game in which the program generates a secret code and the user guesses the code
+ * After each guess, the program displays the results of that gues&mdash;name
+ * the number of characters in the correct position, and the number of additional characters that
+ * are in the secret code and the guess, but are not in the correct position of the guess.
+ *
+ * @version 1.0
+ * @author Isaac Dominguez
+ */
 public class Codebreaker {
 
+     private static final String PROLOGUE_FORMAT = "Pool: %s. Code length: %d%n";
+     private static final String GUESS_PROMPT_FORMAT = "Guess #%d: ";
+     private static final String GUESS_RESULT_FORMAT = "In position: %d. Out of position: %d.%n";
+     private static final String SUCCESS_MESSAGE_FORMAT =
+      "Correct! Secret code (\"%s\") found in %d guesses.%n";
+    private static final String NO_INPUT_MESSAGE = "Unable to read input. Program stopped.";
     private static final String CHARACTER_POOL = "ROYGBIV";
     private static final int CODE_LENGTH =4;
 
-    public static void main(String[] args) {
-      Game game = new Game (CHARACTER_POOL, CODE_LENGTH, new SecureRandom());
-      System.out.printf("Pool: %s. Code length: %d%n", CHARACTER_POOL, CODE_LENGTH);
-      boolean correct =false;
-      Reader input = new InputStreamReader(System.in);
-      BufferedReader reader = new BufferedReader(input);
-      do {
-        try {
-          String text = reader.readLine();
-          Guess guess = game.guess(text);
-          if (guess.getCorrect() == CODE_LENGTH) {
-            System.out.printf("Congradulations! The secret code was %s.%n", game.getCode());
-            correct = true;
-          } else {
-            System.out.printf("Correct: %d. Close %d.%n", guess.getCorrect(), guess.getClose());
-          }
-        } catch (IOException e) {
-          System.out.println("Unable to read input. Program stopped");
-          e.printStackTrace();
-          System.exit(1);
-        } catch (IllegalArgumentException e ) {
-          System.out.println(e.getMessage());
-        }
-      } while (!correct);
+  /**
+   * Entry point for Codebreaker game. Creates an instance of the {@link Game}, and repeatedly takes
+   * user input to submit as a guess, using the {@link Game#guess(String)} method.
+   * @param args Command -line arguments (ignored).
+   */
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      Game game = new Game(CHARACTER_POOL, CODE_LENGTH, new SecureRandom());
+      System.out.printf(PROLOGUE_FORMAT, game.getPool(), game.getLength());
+      //noinspection StatementWithEmptyBody
+      while (!solved(scanner, game)) {}
+    } catch (NoSuchElementException | IllegalStateException e) {
+      System.out.println(NO_INPUT_MESSAGE);
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
 
+  private static boolean solved(Scanner scanner, Game game) {
+    boolean correct = false;
+    try {
+      System.out.printf(GUESS_PROMPT_FORMAT, game.getGuessCount() + 1);
+      String text = scanner.nextLine().trim().toUpperCase();
+      Guess guess = game.guess(text);
+      if (guess.getCorrect() == game.getLength()) {
+        System.out.printf(SUCCESS_MESSAGE_FORMAT, game.getCode(), game.getGuessCount());
+        correct = true;
+      } else {
+        System.out.printf(GUESS_RESULT_FORMAT, guess.getCorrect(), guess.getClose());
+      }
+    } catch (IllegalArgumentException e) {
+      System.out.println(e.getMessage());
+    }
+    return correct;
   }
 
 }
--- src/edu/cnm/deepdive/codebreaker/model/Code.java
+++ src/edu/cnm/deepdive/codebreaker/model/Code.java
@@ -9,10 +9,22 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+/**
+ * Encapsulates a randomly generated secret code, This class also includes the inner  {@code Guess}
+ * class.
+ */
 public class Code {
 
   private final char[] secret;
 
+  /**
+   * Initializes this instance by generating a random {@link String}, of length {@code length} with
+   * characters taken from {@code pool}. A source of randomness must be provided in {@code rng}.
+   *
+   * @param pool Characters allowed in the code.
+   * @param length Number of characters in the code.
+   * @param rng Source of randomness
+   */
   public Code(String pool, int length, Random rng) {
     secret = new char[length];
     for (int i = 0; i < secret.length; i++) {
@@ -25,6 +37,9 @@ public class Code {
     return new String(secret);
   }
 
+  /**
+   * Represents a single attempt to guess a {@link Code}.
+   */
   public class Guess {
 
     private static final String STRING_FORMAT = "{text: \"%s\", correct: %d, close: %d}";
@@ -33,6 +48,12 @@ public class Code {
     private final int correct;
     private final int close;
 
+    /**
+     * Initializes this instance by computing the number of characters in the {@code text} that are
+     * also in the {@link Code}, and are in the same position in both, as well as the number of
+     * additional characters that are in both, but not in the sme position
+     * @param text Guess content
+     */
     public Guess(String text) {
       this.text = text;
       int correct = 0;
@@ -85,14 +106,26 @@ public class Code {
       return String.format(STRING_FORMAT, text, correct, close);
     }
 
+    /**
+     *  Returns the text of the guess.
+     */
     public String getText() {
       return text;
     }
 
+    /**
+     * Returns the number of characters in the guess that are also in the code, which are in the
+     *  same position in both.
+     */
     public int getCorrect() {
       return correct;
     }
 
+    /**
+     * Returns the number of characters in the guess that are also in the code, but which are not in
+     * the same position in both. Note that any characters already matched in the code by
+     * {@link #getCorrect()} are not included in this result.
+     */
     public int getClose() {
       return close;
     }
